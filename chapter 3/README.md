7d884d896adc79f3a7a4c8397dafacf83cc269ec
Denis Jacquemin <denis.jacquemin@gmail.com>
2012-02-23 08:07:15 +0100

    on the way to chapter 3

---
+++ chapter 3/README.md
@@ -0,0 +1,9 @@
+# Chapter 3 - Practical: A Simple Database
+
+- (P)List 
+- global variable 
+- format parameters
+- reading from prompt
+- parse-integer
+
+[Practical: A Simple Database](http://www.gigamonkeys.com/book/practical-a-simple-database.html)
---
+++ chapter 3/records-db.lisp
@@ -0,0 +1,22 @@
+(defvar *db* nil)
+
+(defun make-cd (title artist rating ripped)
+  (list (:title title :artist artist :rating rating :ripped ripped)))
+
+(defun add-record (cd)
+  (push cd *db*))
+
+(defun dumb-db (db)
+  (format t "~{~{~a:~10t~a~%~}~%~}" db))
+
+(defun prompt-read (prompt)
+  (format *query-io* "~a: " prompt)
+  (force-output *query-io*)
+  (read-line *query-io*))
+
+(defun prompt-for-cd ()
+  (make-cd
+    (prompt-read "Title")
+    (prompt-read "Artist")
+    (or (parse-integer (prompt-read "Rating") :junk-allowed t) 0)
+    (prompt-read "Ripped [y/n]")))
